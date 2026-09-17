a8e58ca13e4ea984fd0bbfb02f84e347c9513201
Zaid khan <mkzaid46@gmail.com>
2026-09-17 23:03:48 +0530

    feat: add fetchProductsByFilter route

--- Backend/src/controllers/product.controller.js
+++ Backend/src/controllers/product.controller.js
@@ -9,6 +9,7 @@ export const productPost = async(req,res) => {
             location,
             year,
             categoryId,
+            productDetail
         } = req.body
         const userId = req.user.userId
 
@@ -21,6 +22,7 @@ export const productPost = async(req,res) => {
                         url: image
                     }
                 },
+                productDetail,
                 price,
                 location,
                 year,
@@ -91,4 +93,69 @@ export const fetchAllProducts = async(req,res)=>{
     }
 }
 
-export 
+export const fetchProductsByFilter = async(req,res) => {
+    try{
+        const page = Math.max(Number(req.query.page)||1 , 1)
+        const limit = Math.min(Math.max(Number(req.query.limit)|| 10 , 1),50)
+        const skip = (page-1)*limit
+
+
+        const categoryId = Number(req.params.categoryId)
+        const minPrice = Number(req.query.min_price_) || undefined
+        const maxPrice = Number(req.query.max_price_) || undefined
+        const afterYear = Number(req.query.after_year)|| undefined
+
+        const where = {
+            categoryId,
+            price:{
+                gte: minPrice,
+                lte: maxPrice
+            },
+            year:{
+                gte: afterYear
+            },
+        }
+
+        const [products, totalProducts] = await Promise.all([
+            prisma.product.findMany({
+                skip,
+                take:limit,
+                orderBy: {
+                    createdAt: "desc"
+                },
+                where
+        }),
+        prisma.product.count({
+        where
+        })
+            
+        ])
+        if(products.length ===0 ){
+            return res.status(200).json({
+                success:true,
+                message:"Product not found in this category"
+            })
+        }
+        const totalPages = Math.ceil(totalProducts/limit)
+        return res.status(200).json({
+            success:true,
+            message:"Products found successfully in this category",
+            data: products,
+            pagination: {
+                page,
+                limit,
+                totalProducts,
+                totalPages,
+                hasNextPage: page<totalPages,
+                hasPreviousPage: page>1
+            }
+
+        })
+    }catch(err){
+        console.log("Failed to fetch products by category Internal Error: ",err)
+        return res.status(500).json({
+            success:false,
+            message:"Failed to fetch products by category"
+        })
+    }
+}
--- Backend/src/routers/product.route.js
+++ Backend/src/routers/product.route.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { fetchAllProducts, productPost} from "../controllers/product.controller.js"
+import { fetchAllProducts, fetchProductsByFilter, productPost} from "../controllers/product.controller.js"
 import { authMiddleware } from "../middlewares/auth.middleware.js"
 
 const router = express.Router()
 
 router.post("/product-post",authMiddleware,productPost)
 router.get("/fetch-products",fetchAllProducts)
+router.get("/fetchProductsByFilter/:categoryId",fetchProductsByFilter)
 
 export default router
